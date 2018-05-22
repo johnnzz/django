1c4b2af9f4e37858ad89aa473d06ba400f10abaf
John Navitsky <johnn@cray.com>
2018-05-21 22:45:30 -0700

    updates

--- myblog/tests.py
+++ myblog/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from myblog.models import Post
+import datetime
+from django.utils.timezone import utc
+
 
 class PostTestCase(TestCase):
     fixtures = ['myblog_test_fixture.json', ]
@@ -13,3 +16,39 @@ class PostTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
+
+class FrontEndTestCase(TestCase):
+    fixtures = ['myblog/myblog_test_fixture.json',]
+
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+ 
+        for count in range(1, 11):
+            post = Post(title="Test Post %d Title" % count,
+                text="foo", author=author)
+
+            if count < 6:
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+
+        for count in range(1, 11):
+            title = "Test Post %d Title" % count
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from myblog.models import Post, Category
 
 def index(request):
     return HttpResponse("Hello, world. You're at the myblog index.")
 
+def list_view(request):
+    print("Xrequest",request)
+    #published = Post.objects.exclude(published_date__exact=None)
+    published = Post.objects
+    print("XPUB",published)
+    posts = published.order_by('-published_date')
+    print("XPOSTS",posts)
+    template = loader.get_template('list.html')
+    #context = RequestContext(request, {'posts': posts})
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html") 
+
+def detail_view(request, post_id):
+    #published = Post.objects.exclude(published_date__exact=None)
+    published = Post.objects
+
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'detail.html', context)
